b99cdb7d3e05f3bde3c90bc33e9031e7aacbe32e
varadmondkar <varadmondkar@gmail.com>
2024-08-25 14:55:07 +0530

    Initial Commit

---
+++ Weather UI App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather UI App
+//
+//  Created by Varad Mondkar on 25/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather UI App/Weather_UI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_UI_AppApp.swift
+//  Weather UI App
+//
+//  Created by Varad Mondkar on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_UI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
